16682f78197ef5c41f9412b1ffd1ab0fab1c766b
MaxVonKnobloch <max.vknobloch@gmail.com>
2020-04-14 17:22:42 +0200

    edit_concept_query - tables can be removed

--- tests/util_test.py
+++ tests/util_test.py
@@ -377,6 +377,13 @@ def test_edit_concept_query():
         date_column_id="date_column_id",
         select_ids=["select_1", "select_2"]) == query1_assert_4
 
+    query1_assert_5 = json.load(open("./data_util/concept_query1_assert5.json", 'r'))
+    assert edit_concept_query(
+        query1,
+        concept_id="dataset1.icd",
+        connector_ids="dataset1.icd.arzt_diagnose_icd_code",
+        remove_connector=True) == query1_assert_5
+
 
 @pytest.mark.parametrize("param", valid_date_strings)
 def test_parse_iso_date(param):
