cc18768b928ee414f5775d085c4905245bb6337d
Max von Knobloch <max.knobloch@ingef.de>
2021-07-13 09:58:39 +0200

    bugfix tests

--- tests/test_conquery_ids.py
+++ tests/test_conquery_ids.py
@@ -7,7 +7,7 @@ import cqapi.datasets
 cqapi.datasets.set_dataset_list(["dataset1", "dataset2"])
 
 
-def test_compare_eva_ids():
+def test_compare_conquery_ids():
     assert con.is_same_conquery_id("age", "age")
     assert not con.is_same_conquery_id("age", "not_age")
 
@@ -25,11 +25,11 @@ def test_id_elements_to_id():
     assert con.id_elements_to_id(["foo", "bar"]) == f"foo{con.conquery_id_separator}bar"
 
 
-def test_get_eva_id_element():
+def test_get_conquery_id_element():
     assert con.get_conquery_id_element("foo.bar.foo2.bar2", 0) == "foo"
 
 
-def test_get_eva_id_slice():
+def test_get_conquery_id_slice():
     assert con.get_conquery_id_slice("foo.bar.foo2.bar2") == ["foo", "bar", "foo2", "bar2"]
     assert con.get_conquery_id_slice("foo.bar.foo2.bar2", 2, until_then=True) == ["foo", "bar"]
     assert con.get_conquery_id_slice("foo.bar.foo2.bar2", 2, from_then_on=True) == ["foo2", "bar2"]
@@ -38,14 +38,14 @@ def test_get_eva_id_slice():
         con.get_conquery_id_slice("foo.bar.foo2.bar2", 2, 1)
 
 
-def test_add_dataset_id_to_eva_id():
+def test_add_dataset_id_to_conquery_id():
     with pytest.raises(ValueError):
         con.add_dataset_id_to_conquery_id("foo", "unknown_dataset")
     assert con.add_dataset_id_to_conquery_id("foo", "dataset2") == "dataset2.foo"
-    assert con.add_dataset_id_to_conquery_id("fdb_destatis.foo", "dataset2") == "dataset2.foo"
+    assert con.add_dataset_id_to_conquery_id("dataset1.foo", "dataset2") == "dataset2.foo"
 
 
-def test_remove_dataset_id_from_eva_id():
+def test_remove_dataset_id_from_conquery_id():
     assert con.remove_dataset_id_from_conquery_id("foo") == "foo"
     assert con.remove_dataset_id_from_conquery_id("dataset2.foo") == "foo"
 
