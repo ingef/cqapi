922db9abb61a491750018a01ae5cecf75aeb3829
Max von Knobloch <max.knobloch@ingef.de>
2021-12-03 07:45:20 +0100

    update user editor

--- cqapi/queries/user_editor.py
+++ cqapi/queries/user_editor.py
@@ -6,45 +6,62 @@ from cqapi.queries.translation import translate_query
 from cqapi.api import ConqueryConnection
 from cqapi.conquery_ids import ConqueryIdCollection, contains_dataset_id, \
     is_concept_select, add_dataset_id_to_conquery_id
+import datetime
 
 
-class Editor:
-    """Helper to build and execute queries"""
+def convert_date(date: Optional[str]) -> Optional[str]:
+    if date is None:
+        return None
+
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+        return date
+    except ValueError:
+        try:
+            date_obj = datetime.datetime.strptime(date, '%d.%m.%Y')
+            return date_obj.strftime('%Y-%m-%d')
+        except ValueError:
+            raise ValueError(f"date string must either be of format %Y-%m-%d (e.g. 2020-05-30) "
+                             f"or %d.%m.%Y (e.g. 30.05.2020)")
+
+
+class Query:
 
     def __init__(self, eva: ConqueryConnection):
-        self.conn = eva
         self.query: Optional[QueryObject] = None
+        self.conn = eva
+        self.concepts: Optional[dict] = None
         self.secondary_id: Optional[str] = None
         self.query_id: Optional[str] = None
 
-    def date_restriction(self, date_range: Union[List[str], dict] = None, start_date: str = None, end_date: str = None,
+    def date_restriction(self, start_date: str, end_date: str,
                          label: str = None):
-        self.query = DateRestriction(child=self.query, date_range=date_range, start_date=start_date, end_date=end_date,
-                                     label=label)
+        """Restrict query with a start and end date.
+        E.g. query.date_restriction(start_date="01.01.2020", end_date="31.12.2020)"""
+        self.query = DateRestriction(child=self.query, start_date=convert_date(start_date),
+                                     end_date=convert_date(end_date), label=label)
 
     def negate(self, label: str = None):
+        """Negate the query to get all people that do not fulfill this characteristic"""
         self.query = Negation(child=self.query, label=label)
 
+    def from_existing_query(self, label: str):
+        """Load an already existing query via label"""
+        query_id = self.conn.get_query_id(label=label)
+        self.query = SavedQuery(query_id=query_id)
+
     @staticmethod
-    def _to_query(value: Union[dict, QueryObject, Editor, str]) -> QueryObject:
+    def _to_query(value: Union[dict, QueryObject, Query, str]) -> QueryObject:
         if isinstance(value, str):
             return SavedQuery(query_id=value)
         if isinstance(value, dict):
             return create_query_obj(value)
-        if isinstance(value, Editor):
+        if isinstance(value, Query):
             return value.query
         if isinstance(value, QueryObject):
             return value
 
         raise ValueError(f"Query must be of type Union[dict, str, QueryObject, Editor], not {type(value)}")
-
-    def join_and(self, *queries_to_join):
-        queries = [self._to_query(query) for query in queries_to_join]
-        self.query = AndElement(children=[self.query, *queries])
-
-    def join_or(self, *queries_to_join):
-        queries = [self._to_query(query) for query in queries_to_join]
-        self.query = OrElement(children=[self.query, *queries])
 
     def exclude_from_secondary_id(self) -> None:
         self.query and self.query.exclude_from_secondary_id()
@@ -56,9 +73,10 @@ class Editor:
         if contains_dataset_id(conquery_id):
             return conquery_id
 
-        return add_dataset_id_to_conquery_id(conquery_id=conquery_id, dataset_id=self.conn.get_daraset())
+        return add_dataset_id_to_conquery_id(conquery_id=conquery_id, dataset_id=self.conn.get_dataset())
 
     def add_select(self, *select_ids: str):
+        """Add any select to the query. All query components that can not have this select are ignored"""
         for select_id in select_ids:
             select_id = self._add_dataset(select_id)
             if is_concept_select(select_id):
@@ -67,6 +85,8 @@ class Editor:
                 self.query and self.query.add_connector_select(select_id)
 
     def remove_selects(self, *select_ids: str):
+        """Removes all selects given as input parameters. When none is given, all selects are removed.
+        Examples of select Ids: "icd.exists", "icd.arzt_diagnose_icd_code.anzahl_arztfaelle"""
         select_ids_list = list(select_ids)
         if select_ids_list:
             self.query and self.query.remove_concept_selects(concept_select_ids=select_ids_list)
@@ -86,32 +106,93 @@ class Editor:
     def translate(self, concepts: dict, conquery_conn: ConqueryConnection, return_removed_ids: bool = False) -> \
             Union[Tuple[Union[QueryObject, None], Union[QueryObject, None], ConqueryIdCollection],
                   Tuple[Union[QueryObject, None], Union[QueryObject, None]]]:
-        return translate_query(
-            query=self.query,
-            concepts=concepts,
-            conquery_conn=conquery_conn,
-            return_removed_ids=return_removed_ids
-        )
+        return translate_query(query=self.query,
+                               concepts=concepts,
+                               conquery_conn=conquery_conn,
+                               return_removed_ids=return_removed_ids)
+
+    def clear(self):
+        """Clears Query"""
+        self.query = None
+
+    def join_and(self, *queries_to_join: Union[dict, QueryObject, Query, str]):
+        """current query AND all given objects. Given objects can be query_ids, queries as dicts or Query
+        E.g.
+        query = Query(eva=eva).new(concept_id="alter")
+        query.join_and(
+        """
+        queries = [self._to_query(query) for query in queries_to_join]
+        self.query = AndElement(children=[self.query, *queries])
+
+    def join_or(self, *queries_to_join: Union[dict, QueryObject, Query, str]):
+        """current query AND all given objects. Given objects can be query_ids, queries as dicts or Query"""
+        queries = [self._to_query(query) for query in queries_to_join]
+        self.query = OrElement(children=[self.query, *queries])
+
+    def set_secondary_id(self, secondary_id: str):
+        self.secondary_id = secondary_id
 
     def execute(self, label: str = None) -> str:
-        return self.conn.execute_query(query=self.query, label=label)
+        if not self.query:
+            raise ValueError(f"Can not execute empty query")
 
-    def create_query(self, concept_id: str, concepts: dict, concept_query: bool = False,
-                     connector_ids: List[str] = None,
-                     concept_select_ids: List[str] = None, connector_select_ids: List[str] = None,
-                     filter_objs: List[dict] = None,
-                     exclude_from_secondary_id: bool = None, exclude_from_time_aggregation: bool = None,
-                     date_aggregation_mode: str = None,
-                     start_date: str = None, end_date: str = None):
-        self.query = create_query(concept_id=concept_id,
-                                  concepts=concepts,
-                                  concept_query=concept_query,
-                                  connector_ids=connector_ids,
-                                  concept_select_ids=concept_select_ids,
-                                  connector_select_ids=connector_select_ids,
-                                  filter_objs=filter_objs,
-                                  exclude_from_secondary_id=exclude_from_secondary_id,
-                                  exclude_from_time_aggregation=exclude_from_time_aggregation,
-                                  date_aggregation_mode=date_aggregation_mode,
-                                  start_date=start_date,
-                                  end_date=end_date)
+        if self.secondary_id:
+            query_to_execute = SecondaryIdQuery(root=self.query, secondary_id=self.secondary_id)
+        else:
+            query_to_execute = ConceptQuery(root=self.query)
+
+        self.query_id = self.conn.execute_query(query=query_to_execute, label=label)
+
+        return "Query was executed. Use the download-Method to download the result"
+
+    def download(self, query_id: str = None, use_pandas: bool = True, use_arrow: bool = True):
+        query_id_for_download = query_id if query_id is not None else self.query_id
+
+        if query_id_for_download is None:
+            raise ValueError(f"Nothing executed and no query_id given. Can not download any results")
+
+        self.conn.get_query_result(query_id=query_id_for_download,
+                                   return_pandas=use_pandas,
+                                   download_with_arrow=use_arrow)
+
+
+class Editor:
+    def __init__(self, eva: ConqueryConnection):
+        self.conn = eva
+        self.concepts = eva.get_concepts()
+
+    def _add_dataset(self, conquery_id: str) -> str:
+        if contains_dataset_id(conquery_id):
+            return conquery_id
+
+        return add_dataset_id_to_conquery_id(conquery_id=conquery_id, dataset_id=self.conn.get_dataset())
+
+    def new_query(self, concept_id: str,
+                  connector_ids: List[str] = None,
+                  select_ids: List[str] = None,
+                  start_date: str = None, end_date: str = None) -> Query:
+        """
+        Create query from a eva ids.
+        :param concept_id: E.g. "alter", "geschlecht", "icd", "atc"
+        :param connector_ids: Optional: E.g. "alter.alter", "geschlecht.geschlecht", "icd.arzt_diagnose_icd_code"
+        :param select_ids: Optional: E.g. "alter.alter.alter_select", "geschlecht.geschlecht.geschlecht_text",
+                           icd.arzt_diagnose_icd_code.anzahl_arztfaelle"
+        :param start_date: Optional: E.g. "01.01.2020"
+        :param end_date: Optional: E.g. "31.12.2020"
+        """
+
+        connector_ids = connector_ids or []
+
+        query = Query(eva=self.conn)
+
+        query.query = create_query(concept_id=self._add_dataset(concept_id),
+                                   concepts=self.concepts,
+                                   connector_ids=[self._add_dataset(con_id) for con_id in connector_ids],
+                                   start_date=convert_date(start_date),
+                                   end_date=convert_date(end_date))
+
+        if select_ids:
+            for select_id in select_ids:
+                query.add_select(self._add_dataset(select_id))
+
+        return query
